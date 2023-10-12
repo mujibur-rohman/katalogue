b11f981ed0ecb3db8bc0f361153076dca874a961
mujiburrohman <muji.official06@gmail.com>
2023-10-12 23:23:00 +0700

    update validation and schema

--- api/src/validation/product.validation.ts
+++ api/src/validation/product.validation.ts
@@ -4,6 +4,12 @@ const addProductValidation = Joi.object({
   name: Joi.string().max(255).required(),
   description: Joi.string().required(),
   price: Joi.string().required(),
+  thumbnail: Joi.object(
+    Joi.object().keys({
+      fileName: Joi.string().required(),
+      url: Joi.string().required(),
+    })
+  ),
   photos: Joi.array().items(
     Joi.object().keys({
       fileName: Joi.string().required(),
